1ff604419fe95354f0f5be23e6570f39842a61cb
Luciana Alves <dra.luciana.alves@icloud.com>
2022-12-21 12:21:47 -0300

    Inicio renderizando o elemento na tela.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react';
+import './App.css';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return <div id='page'>
+    <span className='dot'/>
+    </div>;
+}
+
+export default App;
