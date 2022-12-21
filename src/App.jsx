2619749b5dc1a2a7d704d4c30f74d07de08e80e8
Luciana Alves <dra.luciana.alves@icloud.com>
2022-12-21 13:09:01 -0300

    Inicio captura dos valores do novo array formado após cada clique dado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  return <div id='page'>
+const [list, setList] = useState([]);
+const handleClick = (event) => {
+ 
+  const newDot = {
+    clientX: event.clientX,
+    clientY: event.clientY
+  }
+  // copio o que já tinha na minha state com o uso do spread operator e crio um novo array, e passo um novo valor para ele.
+  setList((prev) => [...prev, newDot]);
+}
+  return <div id='page' onClick={handleClick}>
+    {JSON.stringify(list)} 
     <span className='dot'/>
     </div>;
 }
 
 export default App;
+
+// fazendo o stringfy eu consigo ver se está retornando os valores do novo array a partir dos cliques sucessivos na tela.
